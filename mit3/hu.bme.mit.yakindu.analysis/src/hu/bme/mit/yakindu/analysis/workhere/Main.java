f360b2da6fdb514fbfe3ea5e7145da42d4c0a75e
Bencedek <koenig.benedek@gmail.com>
2022-04-11 15:23:44 +0200

    Add files via upload
    2. feladat vége

--- mit3/hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ mit3/hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -1,10 +1,15 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
 import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.sgraph.Transition;
 
 import hu.bme.mit.model2gml.Model2GML;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
@@ -14,7 +19,7 @@ public class Main {
 	public void test() {
 		main(new String[0]);
 	}
-	
+
 	public static void main(String[] args) {
 		ModelManager manager = new ModelManager();
 		Model2GML model2gml = new Model2GML();
@@ -24,14 +29,36 @@ public class Main {
 		
 		// Reading model
 		Statechart s = (Statechart) root;
-		TreeIterator<EObject> iterator = s.eAllContents();
+		TreeIterator<EObject> iterator = s.eAllContents(); 
+		TreeIterator<EObject> nameiterator = s.eAllContents(); 
+		Set<String> deadlocks = new HashSet<String>();
+		Integer nameCounter = 1;
+		while (nameiterator.hasNext()) {
+			EObject content = nameiterator.next();
+			if(content instanceof State) {
+				State state = (State) content;
+				if (state.getName() == "") {
+					state.setName("Untitled State " + nameCounter++);
+					System.out.println("New name: " + state.getName());
+				}					
+			}
+		}
 		while (iterator.hasNext()) {
 			EObject content = iterator.next();
 			if(content instanceof State) {
 				State state = (State) content;
-				System.out.println(state.getName());
+				if (state.getOutgoingTransitions().size() == 0) {
+					deadlocks.add(state.getName());
+				} else {
+					EList <Transition> tr = state.getOutgoingTransitions();
+					for(Transition t: tr) {
+						System.out.println(state.getName() + " -> " + t.getTarget().getName());
+					}
+				}
 			}
 		}
+		System.out.println("Deadlocks:" + deadlocks);
+		
 		
 		// Transforming the model into a graph representation
 		String content = model2gml.transform(root);
